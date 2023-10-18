40e069265942b41a4ce2ff25d225eb2cc81f7868
Nathan Marianovsky <nathanmarianovsky@manualmath.com>
2023-10-18 03:59:20 -0400

    getBook Update
    This fixes a good portion of the previous setup in order to properly extract info on a book from goodreads. Some attributes returned for getBook have been removed if the information could no longer be extracted while others have been added as new. A future commit on the documentation will explain everything.

--- src/api/get-book.ts
+++ src/api/get-book.ts
@@ -1,103 +1,98 @@
-import { element } from '../parsers/element'
-import cover from '../utils/cover'
-import fetch from '../utils/fetch'
+import { element } from '../parsers/element';
+import cover from '../utils/cover';
+import fetch from '../utils/fetch';
 
 type Params = {
-  url?: string
-  isbn?: string
+  url?: string,
+  isbn?: string,
   isbn13?: string
-}
+};
 
 type Book = {
-  id: string
-  url: string
-  title: string
-  originalTitle: string | null
-  originalUrl: string | null
-  author: string | null
-  description: string | null
-  coverSmall: string | null
-  coverLarge: string | null
-  isbn: string | null
-  isbn13: string | null
-  pages: number | null
-  rating: number | null
-  ratingCount: number | null
-  reviewsCount: number | null
-  language: string | null
-  genres: string[][] | null
-  altCovers: string[] | null
-}
+  id: string,
+  url: string,
+  title: string,
+  originalTitle: string | null,
+  bookLinks: string[] | null,
+  authors: string[] | null,
+  description: string | null,
+  coverSmall: string | null,
+  coverLarge: string | null,
+  asin: string | null,
+  isbn: string | null,
+  isbn13: string | null,
+  media: string | null,
+  pages: number | null,
+  publicationYear: number | null,
+  publisher: string | null,
+  rating: number | null,
+  ratingCount: number | null,
+  reviewsCount: number | null,
+  language: string | null,
+  genres: string[] | null
+};
 
 export default async function getBook(params: Params): Promise<Book> {
   if (!params.isbn13 && !params.isbn && !params.url) {
-    throw new Error('No params provided')
+    throw new Error('No params provided');
   }
 
   const url =
     params.url ||
     'https://www.goodreads.com/search?q=' +
-      encodeURIComponent(params.isbn13 || params.isbn)
+      encodeURIComponent(params.isbn13 || params.isbn);
 
-  const { document, responseUrl } = await fetch(url)
+  const { document, responseUrl } = await fetch(url);
 
-  const el = element(document)
+  const el = element(document);
 
-  function dataBoxValue(query: string) {
-    const nodes = el.queryAll('div')
-    const tr = Array.from(nodes).find((node) =>
-      node?.textContent?.toLowerCase().includes(query)
-    )
-
-    if (tr && tr.parentNode) {
-      const td = tr.parentNode.querySelector('.infoBoxRowItem')
-      return element(td).text()
-    }
-    return null
+  function singleFind(container: string, searchItem: string) {
+    const pubIndex = searchStr.indexOf(searchItem) + searchItem.length,
+      containerSub = container.substring(pubIndex);
+    return containerSub.substring(0, containerSub.indexOf('"'));
   }
 
-  function genres() {
-    const nodes = el.queryAll('.brownBackground')
-    const genresBox = Array.from(nodes).find((node) =>
-      node?.textContent?.toLowerCase().includes('genres')
-    )?.parentElement?.parentElement
-
-    if (!genresBox) return
-
-    const genres = Array.from(
-      genresBox.querySelectorAll('.elementList .left')
-    ).map((node) => {
-      return Array.from(node.querySelectorAll('.bookPageGenreLink')).map((a) =>
-        a?.textContent?.trim().escape()
-      )
-    })
-    return genres
+  function multipleFind(container: string, searchItem: string) {
+    const indices = [...container.matchAll(new RegExp(searchItem, 'gi'))].map(a => a.index),
+      arr = [];
+    indices.forEach(ind => {
+      let str = container.substring(ind + searchItem.length);
+      arr.push(str.substring(0, str.indexOf('"')));
+    });
+    return arr;
   }
+
+  const infoScriptText = el.query('[type="application/ld+json"]')?.text(),
+    infoScript = infoScriptText !== undefined ? JSON.parse(infoScriptText) : {},
+    searchStr = el.query('#__NEXT_DATA__').text(),
+    originalTitleTest = singleFind(searchStr, ',"originalTitle":"'),
+    asinTest = singleFind(searchStr, ',"asin":"'),
+    publisherTest = singleFind(searchStr, ',"publisher":"');
 
   const result = {
     id: responseUrl?.split('show/')[1]?.split('-')[0],
     url: el.query('[hreflang="en"]')?.attr('href') || responseUrl,
-    title: el.query('.Text__title1')?.text(),
-    originalTitle: dataBoxValue('original title'),
-    originalUrl: dataBoxValue('url'),
-    author: el.query('[data-testid=name]')?.text(),
-    description: el
-      .query('.DetailsLayoutRightParagraph__widthConstrained')
-      ?.text(),
-    coverSmall: el.query('.BookCover__image>div>img')?.attr('src'),
-    coverLarge: cover(el.query('.BookCover__image>div>img')?.attr('src')),
-    isbn: dataBoxValue('isbn')?.split('(')[0]?.escape(),
-    isbn13: el.query('[itemprop="isbn"]')?.text(),
-    pages: el.query('.pagesFormat')?.text().toInt(),
-    rating: el.query('.RatingStatistics__rating')?.text().toFloat(),
-    ratingCount: el.query('[data-testid=ratingsCount]')?.text().toFloat(),
-    reviewsCount: el.query('[data-testid=reviewsCount]')?.text().toFloat(),
-    language: el.query('[itemprop="inLanguage"]')?.text(),
-    genres: genres(),
-    altCovers: Array.from(el.queryAll('.otherEdition img')).map((img) =>
-      cover(element(img)?.attr('src'))
-    ),
+    title: infoScript.name || "",
+    originalTitle: originalTitleTest !== 'ops' ? originalTitleTest : null,
+    bookLinks: multipleFind(searchStr, '"__typename":"BookLink","name":"')
+      .map(source => singleFind(searchStr, '"__typename":"BookLink","name":"' + source + '","url":"').replace(/\\/gi, "/")),
+    authors: infoScript.author?.map(authorItem => authorItem.name) || [],
+    description: el.query('.DetailsLayoutRightParagraph__widthConstrained')?.text() || null,
+    coverSmall: el.query('.BookCover__image>div>img')?.attr('src') || null,
+    coverLarge: cover(el.query('.BookCover__image>div>img')?.attr('src')) || null,
+    asin: asinTest !== ':{' ? asinTest : null,
+    isbn: infoScript.isbn?.substring(3).toInt() || null,
+    isbn13: infoScript.isbn?.toInt() || null,
+    media: infoScript.bookFormat || null,
+    pages: infoScript.numberOfPages || null,
+    publicationYear: el.query('.FeaturedDetails [data-testid="publicationInfo"]')?.text().split(" ").reverse()[0].toInt() || null,
+    publisher: publisherTest !== 'geProps' ? publisherTest : null,
+    rating: infoScript.aggregateRating?.ratingValue || null,
+    ratingCount: infoScript.aggregateRating?.ratingCount || null,
+    reviewsCount: infoScript.aggregateRating?.reviewCount || null,
+    language: infoScript.inLanguage || null,
+    genres: multipleFind(searchStr, '"__typename":"Genre","name":"')
   }
 
-  return result as any
+  return result as any;
 }
