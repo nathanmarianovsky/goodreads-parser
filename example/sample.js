70dab0e0a9cb985eb5e99f232fcee28b8fd95b91
Nathan Marianovsky <nathanmarianovsky@manualmath.com>
2023-10-19 15:38:06 -0400

    Cleaning Up

--- example/sample.js
+++ example/sample.js
@@ -1,13 +1,9 @@
-const GoodReadsParser = require('../build/')
+const GoodReadsParser = require('../build/');
 
 ;(async () => {
   try {
-    // await GoodReadsParser.searchBooks({ q: 'dark matter' })
-    // await GoodReadsParser.searchBooks({ q: 'პურის ყანა' })
-    const result = await GoodReadsParser.searchBooks({ q: 'Dark', page: 1 })
-    // const result = await GoodReadsParser.getBook({ isbn: '1524759783' })
-    console.log('result:', result)
-  } catch (error) {
-    console.log('error', error)
-  }
-})()
+    const result = await GoodReadsParser.searchBooks({ q: 'Dark' });
+    // const result = await GoodReadsParser.getBook({ url: 'https://www.goodreads.com/book/show/36262331-the-three-body-problem' })
+    console.log('result:', result);
+  } catch (error) { console.log('error', error); }
+})();
--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,6 @@
 interface String {
-  toInt(): number
-  toFloat(): number
-  between(start: string, end: string): string
+  toInt(): number,
+  toFloat(): number,
+  between(start: string, end: string): string,
   escape(): string
 }
